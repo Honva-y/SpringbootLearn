f738996896bbc767dbe19b7c1603a1a4c1592aa7
TickFxh <334949356@qq.com>
2017-12-08 15:08:26 +0800

    all code,first

---
+++ src/main/java/com/honva/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.honva.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/hello")
+	public String hello(){
+		return "hello!!!!!!!!!!";
+	}
+}
